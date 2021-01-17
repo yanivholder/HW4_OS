a8a5b51b4056c9de2003f94e0f1082194661ab66
Yaniv Holder <yaniv.holder@outlook.com>
2021-01-17 11:39:56 +0000

    malloc 2 fixes

--- wet/malloc_2.cpp
+++ wet/malloc_2.cpp
@@ -16,46 +16,47 @@ typedef struct MallocMetaData {
 MMD head = {0, false, nullptr, nullptr};
 MMD* head_p = &head;
 
-static MMD* find_first_available(size_t size, bool* really_available){
+static MMD* find_first_available(size_t size, bool* really_available) {
     MMD* curr = head_p;
     while (curr->next != nullptr) {
         curr = curr->next;
         if (curr->is_free == false)
             continue;
-        else{ //curr is available!
+        else { //curr is available!
             if (curr->size >= size){
                 *really_available = true;
                 return curr;
-            } else //not enough space in curr
-                continue;
+            }
         }
     }
     return curr; //means we did not find available space, return value is last node in list
 }
 
 
-void* smalloc(size_t size){
-
+void* smalloc(size_t size) {
+    if(size == 0 || size > MAX_SIZE){
+        return nullptr;
+    }
     bool really_available = false;
     MMD* first_available = find_first_available(size, &really_available);
     if (really_available){
         first_available->is_free = false;
-        return (void*)(&first_available[1]); //skip meta data and give user the data pointer
+        return (void*)(&first_available[1]); // skip meta data and give user the data pointer
     }
-    else{ //'first_available' wasn't really available, simply last in linked list
-        if(size == 0 || size > MAX_SIZE){
-            return nullptr;
-        }
+    else { // 'first_available' wasn't really available, simply last in linked list
         void* meta_address = sbrk(sizeof(MMD));
-        void* data_address = sbrk(size);
-        if (meta_address == (void*)(-1) || data_address == (void*)(-1)){
+        if (meta_address == (void*)(-1))
             return nullptr;
-        } else{
-            MMD* last_in_old_list = first_available; // the var's meaning changed, this is just to make code clear
-            *((MMD*)(meta_address)) = MMD{size, false, nullptr, last_in_old_list}; //first available was just the last in linked list
-            last_in_old_list->next = (MMD*)(meta_address);
-            return data_address;
+        void* data_address = sbrk(size);
+        if (data_address == (void*)(-1)) {
+            sbrk(-sizeof(MMD)); // cancel prev brk
+            return nullptr;
         }
+
+        MMD* last_in_old_list = first_available; // the var's meaning changed, this is just to make code clear
+        *((MMD*)meta_address) = MMD{size, false, nullptr, last_in_old_list}; //first available was just the last in linked list
+        last_in_old_list->next = (MMD*)meta_address;
+        return data_address;
     }
 }
 
@@ -72,29 +73,27 @@ void sfree(void* p) {
     if (p == nullptr)
         return;
 
-    MMD* p_MMD = (MMD*)p;
-    MMD* mmd_p = &p_MMD[-1];
+    MMD* mmd_p = &((MMD*)p)[-1];
     if (mmd_p->is_free)
         return;
 
     mmd_p->is_free = true;
-    mmd_p->prev = head_p;
-    mmd_p->next = head_p->next;
-    head_p->next = mmd_p;
+//    mmd_p->prev = head_p;
+//    mmd_p->next = head_p->next;
+//    head_p->next = mmd_p;
 }
 
 void* srealloc(void* oldp, size_t size) {
     if (size == 0 || size > MAX_SIZE)
         return nullptr;
 
-    MMD* oldp_MMD = (MMD*)oldp;
-    MMD* old_mmd_p = &oldp_MMD[-1];
-    if (old_mmd_p->size > size)
+    MMD* old_mmd_p = &((MMD*)oldp)[-1];
+    if (old_mmd_p->size > size) // TODO check if also equal
         return oldp;
 
     void* new_adress_p = smalloc(size);
     memcpy(new_adress_p, oldp, old_mmd_p->size);
-    sfree(oldp);
+    old_mmd_p->is_free = true;
 }
 
 size_t _num_free_blocks() {
